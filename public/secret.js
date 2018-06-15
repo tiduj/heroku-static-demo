957c0b8e33ce99f07bba8953189920dd4327fad2
tiduj <25076311+tiduj@users.noreply.github.com>
2018-06-15 11:39:08 +0200

    Update secret.js

--- public/secret.js
+++ public/secret.js
@@ -1,2 +1,2 @@
 scout.config.urls.apiRoot = "https://maju1.herokuapp.com/api/v1/";
-scout.config.urls.domainRoot = "https://maju1.herokuapp.com/";
+scout.config.urls.domainRoot = "https://maju1.herokuapp.com";
